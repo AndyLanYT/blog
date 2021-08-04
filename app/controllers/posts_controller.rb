94dd5efc15c24be2bc4b695195d40ba27abc8ba6
AndyLanYT <AndyLanYT@gmail.com>
2021-08-04 17:20:38 +0300

    Posts controller and views

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,55 @@
+class PostsController < ApplicationController
+    # layout false
+    skip_before_action :verify_authenticity_token
+    before_action :set_post, only: [:edit, :update, :show, :destroy]
+
+    def index
+        @posts = Post.all
+    end
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post = Post.new(posts_params)
+        if @post.save
+            flash[:notice] = "Post was created!!"
+            redirect_to post_path(@post)
+        else
+            flash[:notice] = "Fatal error!!"
+        end
+    end
+
+    def show
+    end
+    
+    def edit
+    end
+
+    def update
+        if @post.update(posts_params)
+            flash[:notice] = "Post was updated!"
+            redirect_to post_path(@post)
+        else
+            flash[:notice] = "Post was not updated! =("
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @post.destroy
+        flash[:notice] = "Post was destroyed!"
+        redirect_to posts_path
+    end
+
+
+    private
+    def posts_params
+        params.require(:post).permit(:title, :description)
+    end
+
+    def set_post
+        @post = Post.find(params[:id])
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # resources :users, exclude: [:index]
+  # get '/post', to
+
+  resources :posts
+
+  # get '/posts/new', to: 'posts#new'
+
+  # post '/posts/create', to: 'posts#create'
+
 end
