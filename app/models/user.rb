85a79bc4cbcb7f7c7fd49d8e34489c1f9c25f138
AndyLanYT <AndyLanYT@gmail.com>
2021-08-06 11:47:37 +0300

    Add CanCanCan gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # validates :username, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
   # validates :password, presence: true, length: {in: 6..20}
   validates :email, presence: true, uniqueness: true
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,42 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  context 'validation tests' do
-    it 'ensures title presence' do
-      post = described_class.new(description: 'Description').save
-      expect(post).to eq(false)
-    end
+  it 'ensures title presence' do
+    post = described_class.new(description: 'Description')
+    expect(post).to_not be_valid
 
-    it 'has a title length is equal or greater than 2' do
-      post = described_class.new(title: '', description: 'Just a description')
-      expect(post).not_to be_valid
+    post.title = 'Title'
+    expect(post).to be_valid
+  end
+  
+  it 'ensures description presence' do
+    post = described_class.new(title: 'Title')
+    expect(post).to_not be_valid
 
-      post.title = 'TT'
-      expect(post).to be_valid
-    end
+    post.description = 'Description'
+    expect(post).to be_valid
+  end
 
-    it 'ensures description presence' do
-      post = described_class.new(title: 'Title').save
-      expect(post).to eq(false)
-    end
+  it 'has a title length equal at least 2' do
+    post = described_class.new(title: '', description: 'Just a description')
+    expect(post).not_to be_valid
 
-    it 'has a description length is between 5 and 100' do
-      post = described_class.new(title: 'Just a title', description: '')
-      expect(post).not_to be_valid
+    post.title = 'TT'
+    expect(post).to be_valid
+  end
+  
+  it 'has a description length between 5 and 100' do
+    post = described_class.new(title: 'Just a title', description: '')
+    expect(post).not_to be_valid
 
-      post.description = 'Text!'
-      expect(post).to be_valid
+    post.description = 'Text!'
+    expect(post).to be_valid
 
-      post.description = 'abcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghij'
-      expect(post).to be_valid
+    post.description = '*' * 100
+    expect(post).to be_valid
 
-      post.description = 'abcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghija'
-      expect(post).not_to be_valid
-    end
+    post.description = '*' * 101
+    expect(post).not_to be_valid
+  end
 
-    it 'is saved successfully' do
-      post = described_class.new(title: 'Title', description: 'Description').save
-      expect(post).to eq(true)
-    end
+  it 'is saved successfully' do
+    post = described_class.new(title: 'Title', description: 'Description').save
+    expect(post).to eq(true)
   end
 end
