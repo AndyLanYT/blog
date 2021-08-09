7f072ca9fe51f279bcd9724224d71fc14bd47f56
AndyLanYT <AndyLanYT@gmail.com>
2021-08-09 10:48:26 +0300

    Commentsgit add .!

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,23 +3,26 @@
 class Ability
   include CanCan::Ability
 
-  def guest_can
-    can :read, Post
-  end
-  
-  def user_can(user_id)
-    can :manage, Post, user_id: user_id
-  end
-
-  def admin_can
-    can :manage, :all
-  end
-
   def initialize(user)
-    user ||= User.new
+    user ||= nil
 
     guest_can
     user_can(user.id) if user.user_role?
     admin_can if user.admin?
+  end
+
+  private
+
+  def guest_can
+    can :read, [Post, Comment]
+  end
+  
+  def user_can(user_id)
+    can :manage, [Post, Comment], user_id: user_id
+    # can :manage, :all
+  end
+
+  def admin_can
+    can :manage, :all
   end
 end
