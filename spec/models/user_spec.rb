3bf604038d77892b57c4bf473cd6ec3f1f29aa4e
AndyLanYT <AndyLanYT@gmail.com>
2021-08-10 15:07:58 +0300

    Smth is wrong with spec...

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # it 'has an email' do
-  #   user = described_class.new(id: 1, email: nil, password: 'password')
-  #   expect(user).to_not be_vaild
+  it 'has an email' do
+    should validate_presence_of :email
+    # user = described_class.new(email: nil)
+    # expect(user).to_not be_vaild
+    
+    # user.email = 'email@example.com'
+    # expect(user).to be_vaild
+  end
 
-  #   user.email = 'email@example.com'
-  #   expect(user).to be_vaild
-  # end
+  it 'has many posts' do
+    should have_many :posts
+  end
 end
